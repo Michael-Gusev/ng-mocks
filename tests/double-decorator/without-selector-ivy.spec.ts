f36bf64ff55bebad008c41ef1d391fb14201ec19
MG <m@sudo.eu>
2020-12-13 19:28:06 +0100

    build: junit fix

--- tests/double-decorator/without-selector-ivy.spec.ts
+++ tests/double-decorator/without-selector-ivy.spec.ts
@@ -40,6 +40,12 @@ class MyComponent {
 class ModuleWithComponent {}
 
 describe('double-decorator-ivy:without-selector', () => {
+  // Because of junit issue we need to return before beforeEach
+  // https://github.com/karma-runner/karma-junit-reporter/issues/186
+  if (!(core as any).ɵivyEnabled) {
+    return;
+  }
+
   beforeEach(() => {
     if (!(core as any).ɵivyEnabled) {
       pending('ivy fails differently');
